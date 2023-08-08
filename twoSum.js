165c1a1cf30d674b83c579dbc74aa6c51004ee3e
Prasuk Jain <prasukj02@gmail.com>
2023-08-08 11:04:15 +0530

    first commit

--- twoSum.js
+++ twoSum.js
@@ -1,10 +1,25 @@
-function twoSum(arr, target){
-    for(let i = 0; i<arr.length; i++){
-        for(let j = i + 1; j<arr.length; j++){
-            if(arr[i] + arr[j] == target){
-                return [i, j]
-            }
+// Time complexity: O(n^2)
+// function twoSum(arr, target){
+//     for(let i = 0; i<arr.length; i++){
+//         for(let j = i + 1; j<arr.length; j++){
+//             if(arr[i] + arr[j] == target){
+//                 return [i, j]
+//             }
+//         }
+//     }
+//     return -1
+// }
+
+// Time complexity: O(n)
+function twoSum(nums, target){
+    let map = {}
+    for(let i = 0; i<nums.length; i++){
+        let rest = target - nums[i]
+        if(rest in map){
+            return [map[rest], i]
         }
+        map[nums[i]] = i;
     }
-    return -1
 }
+
+console.log(twoSum([3, 2, 4, 3, 5], 9))
