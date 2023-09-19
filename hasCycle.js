1537336dba6393bbe9436f1e452dfeb1429b6492
Prasuk Jain <prasukj02@gmail.com>
2023-09-19 20:09:18 +0530

    141. hasCycle

---
+++ hasCycle.js
@@ -0,0 +1,15 @@
+// 141. https://leetcode.com/problems/linked-list-cycle/
+var hasCycle = function(head) {
+    
+    let fast = head
+    let slow = head
+
+    while(fast && fast.next){
+        slow = slow?.next
+        fast = fast?.next?.next
+        if(fast === slow){
+            return true
+        }
+    }
+    return false
+};
